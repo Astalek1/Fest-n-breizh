5ceeec4dc64c228b55312fa8df86bf90d19ad91d
kévin goujon <goujon.kevin@hotmail.fr>
2024-07-03 12:04:08 +0200

    finalisation du menu déroulant pour toutes les pages

--- JAVASCRIPT/menu.js
+++ JAVASCRIPT/menu.js
@@ -1,8 +1,12 @@
-function toggleMenu() {
-    let sousMenu = document.getElementById('sousMenuA');
+function toggleMenu(menuId, parentId) {
+    let sousMenu = document.getElementById(menuId);
+    let parent = document.getElementById(parentId);
     if (sousMenu.classList.contains('active')) {
         sousMenu.classList.remove('active');
     } else {
         sousMenu.classList.add('active');
+        sousMenu.style.witdh = `${parent.offsetWidth}px`;
     }
 }
+
+
