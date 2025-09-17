88e23ed8f40b3cd65ab62b9e3f2097f458c89527
kévin goujon <goujon.kevin@hotmail.fr>
2025-09-17 21:47:25 +0200

    ajout models Editions

---
+++ backend/models/Editions.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+const editionsSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    affiche: {type: String, required: true},
+    artistes: [
+                {
+                name: {type: String, required: true},
+                description: {type: String, required: true},
+                media: {type: String, required: true} 
+                }
+            ],
+    invites: [
+            {
+                name: {type: String, required: true},
+                description: {type: String, required: true},
+                media: {type: String, required: true} 
+            }
+        ],
+    },
+)
---
+++ backend/routes/editions.js
@@ -0,0 +1,17 @@
+import express from "express"
+import auth from "../middleware/auth"
+import  multer from"../middleware/multer"
+import resizeImage from "../middleware/resizeImage"
+import * as userCtrl from "../controllers/editions.js"
+
+const router = express.Router();
+
+router.get("/");
+router.get("/:id");
+
+router.post("/", auth, multer, resizeImage);
+
+router.put("/:id", auth, multer, resizeImage);
+
+router.delete("/:id", auth);
+
