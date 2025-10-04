c74100cdf09fee1212b52a6a17ec7266d6464f98
kévin goujon <goujon.kevin@hotmail.fr>
2025-10-04 15:38:14 +0200

    test gallery.js

--- backend/models/Gallery.js
+++ backend/models/Gallery.js
@@ -3,18 +3,13 @@ import mongoose from "mongoose";
 const gallerySchema = mongoose.Schema({
   title: { type: String, required: true },
   url: { type: String, required: true }, // version large
-  urlSmall: { type: String, required: true }, // version réduite
-  mediaFileId: { type: String, required: true },
-  mediaFileIdSmall: { type: String, required: true },
+  urlSmall: { type: String }, // version petite
+  mediaFileId: { type: String, required: true }, // fileId de la version large
+  mediaFileIdSmall: { type: String }, // fileId de la version petite
   alt: { type: String, required: true },
   caption: { type: String },
-  type: {
-    type: String,
-    enum: ["photo", "poster"],
-    required: true,
-  },
+  type: { type: String, enum: ["photo", "poster"], required: true },
 });
 
 const Gallery = mongoose.model("Gallery", gallerySchema);
-
 export default Gallery;
