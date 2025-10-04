f078bab0febed5bab6fdebdfef7a0210efef016c
kévin goujon <goujon.kevin@hotmail.fr>
2025-10-04 17:43:57 +0200

    modif isfileInUse

--- backend/utils/isFileInUse.js
+++ backend/utils/isFileInUse.js
@@ -1,18 +1,28 @@
+import Gallery from "../models/Gallery.js";
 import Artist from "../models/Artists.js";
 import Guest from "../models/Guests.js";
 // Plus tard : Edition si besoin
 
 export const isFileInUse = async (fileId) => {
-  console.log("🧩 Vérif utilisation fichier :", fileId, "→", inUse);
+  try {
+    // Vérifie d'abord dans Gallery
+    const galleryUse = await Gallery.findOne({
+      $or: [{ mediaFileIdLarge: fileId }, { mediaFileIdSmall: fileId }],
+    });
 
-  // Vérifie dans Artistes
-  const artistUse = await Artist.findOne({ mediaFileId: fileId });
-  if (artistUse) return true;
+    // Vérifie dans Artistes
+    const artistUse = await Artist.findOne({ mediaFileId: fileId });
 
-  // Vérifie dans Invités
-  const guestUse = await Guest.findOne({ mediaFileId: fileId });
-  if (guestUse) return true;
+    // Vérifie dans Invités
+    const guestUse = await Guest.findOne({ mediaFileId: fileId });
 
-  // Si rien trouvé → pas utilisé
-  return false;
+    const inUse = !!(galleryUse || artistUse || guestUse);
+
+    console.log("🧩 Vérif utilisation fichier :", fileId, "→", inUse);
+
+    return inUse;
+  } catch (error) {
+    console.error("Erreur dans isFileInUse :", error.message);
+    return false;
+  }
 };
