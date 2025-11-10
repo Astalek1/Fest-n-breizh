2754964beb70beb94388d7e0a0d6e9bba0404291
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-10 15:09:23 +0100

    test

--- backend/controllers/guests.js
+++ backend/controllers/guests.js
@@ -166,6 +166,8 @@ export const updateGuest = async (req, res, silent = false) => {
     }
 
     const body = req.body.guest ? JSON.parse(req.body.guest) : req.body;
+
+    if (!body.media && req.body.media) body.media = req.body.media;
     if (!body.mediaType && req.body.mediaType) body.mediaType = req.body.mediaType;
 
     // --- 1) Champs textuels ---
