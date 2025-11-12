9d562edc54111ec85d2b08330eb1ddaced7ee758
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-12 19:10:56 +0100

    modifs

--- backend/middleware/resizeImage.js
+++ backend/middleware/resizeImage.js
@@ -94,6 +94,8 @@ export default async function resizeImage(req, res, next) {
   try {
     if (req.file?.buffer) {
       await processOne(req.file, inferType(req, req.file));
+      if (file.bufferLarge) file.buffer = file.bufferLarge; // test
+
       return next();
     }
 
