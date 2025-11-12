386f826d4e1509973ac33d7dba4d98cbf8b296ff
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-12 12:39:05 +0100

    supréssion put artists et guests

--- backend/routes/editions.js
+++ backend/routes/editions.js
@@ -47,14 +47,6 @@ router.post(
   editionsCtrl.addGuestToEdition
 );
 
-router.put(
-  "/:editionId/guests/:guestId",
-  auth,
-  upload.single("media"),
-  resizeImage,
-  editionsCtrl.updateEdition
-);
-
 router.delete("/:editionId/guests/:guestId", auth, guestsCtrl.deleteGuest);
 
 // === GESTION DES ARTISTES D'UNE ÉDITION ===
@@ -64,14 +56,6 @@ router.post(
   upload.fields([{ name: "media", maxCount: 10 }]),
   resizeImage,
   editionsCtrl.addArtistToEdition
-);
-
-router.put(
-  "/:editionId/artists/:artistId",
-  auth,
-  upload.single("media"),
-  resizeImage,
-  editionsCtrl.updateEdition
 );
 
 router.delete("/:editionId/artists/:artistId", auth, artistsCtrl.deleteArtist);
