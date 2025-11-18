8d993aa172b80248fa460a8249629ed930968815
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-18 16:15:28 +0100

    netoyage routes et ping mongodb

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,11 +3,6 @@ import * as userCtrl from "../controllers/user.js";
 
 const router = express.Router();
 
-router.post("/debug", (req, res) => {
-  console.log("DEBUG BODY :", req.body);
-  res.json(req.body);
-});
-
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
