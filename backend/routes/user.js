8792d6ed0d33037d7891a4d38a5f2f8bcdd30d23
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-18 14:41:42 +0100

    test

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,11 @@ import * as userCtrl from "../controllers/user.js";
 
 const router = express.Router();
 
+router.post("/debug", (req, res) => {
+  console.log("DEBUG BODY :", req.body);
+  res.json(req.body);
+});
+
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
