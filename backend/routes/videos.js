37955136dfe91b0d382cf22deb37c5d33c2e7408
kévin goujon <goujon.kevin@hotmail.fr>
2025-11-18 14:25:31 +0100

    ajout multer.none

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -1,5 +1,6 @@
 import express from "express";
 import auth from "../middleware/auth.js";
+import multer from "../middleware/multer.js";
 import * as videosCtrl from "../controllers/videos.js";
 
 const router = express.Router();
@@ -7,9 +8,9 @@ const router = express.Router();
 router.get("/", videosCtrl.getAllVideos);
 router.get("/:id", videosCtrl.getOneVideo);
 
-router.post("/", auth, videosCtrl.newVideo);
+router.post("/", auth, multer.none(), videosCtrl.newVideo);
 
-router.put("/:id", auth, videosCtrl.updateVideo);
+router.put("/:id", auth, multer.none(), videosCtrl.updateVideo);
 
 router.delete("/:id", auth, videosCtrl.deleteVideo);
 
