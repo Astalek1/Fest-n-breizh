a260a46ca598325225fc2ea2986c0c46a8a6bb0e
kévin goujon <goujon.kevin@hotmail.fr>
2025-12-02 12:30:41 +0100

    ajout des route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Editions/:editionId" element={<Editions />} />
+        <Route path="/Gallery/photos" element={<GalleryPhoto />} />
+        <Route path="/Gallery/affiches" element={<GalleryPoster />} />
+        <Route path="/Video" element={<Video />} />
+        <Route path="/Links" element={<Links />} />
+        <Route path="/Partners" element={<Partners />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
