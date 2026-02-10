303a0fd1109e4f5b9dcc73c45b2adb37fd06f6e5
kévin goujon <goujon.kevin@hotmail.fr>
2026-02-10 13:05:14 +0100

    application et modification du scss

--- frontend/src/pages/Videos/Videos.jsx
+++ frontend/src/pages/Videos/Videos.jsx
@@ -1,7 +1,44 @@
-//import './Videos.scss'
+import './Videos.scss'
+import { useState, useEffect } from 'react'
 
 function Videos() {
-  return <>Nos Vidéos</>
+  const [videos, setvideos] = useState([])
+
+  useEffect(() => {
+    fetch('https://fnb-backend.dokku.festnbreizh.bzh/api/videos')
+      .then((res) => res.json())
+      .then((data) => setvideos(data))
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <>
+      <div className="video__intro">
+        <h1 className="video__intro--title">Nos vidéo sur Youtube</h1>
+
+        <p className="video__intro--txt">
+          Retrouvez ici toutes les actualités du festival : nouveautés,
+          événements, invités, informations importantes et moments forts à
+          venir.
+        </p>
+      </div>
+      <div className="videos">
+        {videos.map((item) => (
+          <article className="videos__content" key={item._id}>
+            <h2 className="videos__title">{item.title}</h2>
+            <p className="videos__txt">{item.description}</p>
+
+            <iframe
+              className="videos__windows"
+              src={item.url.replace('watch?v=', 'embed/')}
+              title={item.title}
+              allowFullScreen
+            />
+          </article>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Videos
