7f9ab3d480daa19f118071b583ebd6d9ed9a3344
kévin goujon <goujon.kevin@hotmail.fr>
2026-02-12 00:51:50 +0100

    modification texte de bordures

--- frontend/src/pages/Videos/Videos.jsx
+++ frontend/src/pages/Videos/Videos.jsx
@@ -16,7 +16,24 @@ function Videos() {
       <div className="video__intro">
         <h1 className="video__intro--title">Nos vidéo sur Youtube</h1>
 
-        <p className="video__intro--txt">texte a venir.</p>
+        <p className="video__intro--txt">
+          Fest’n Breizh, c’est aussi une diffusion de la musique bretonne hors
+          des parquets !<br /> À travers deux formats, notre association diffuse
+          sur les principales plateformes internet un échantillon de cet univers
+          musical riche.
+          <br /> Des digressions musicales d’abord donnent la parole aux
+          artistes et musiciens, et cela passe aussi par le partage de leur
+          culture commune, leurs inspirations parfois surprenantes et leur
+          rapport au public.
+          <br /> Ensuite, des captures de vidéos live donnent à voir les
+          performances des artistes que nous programmons, nous et nos
+          partenaires, et cela chaque année. <br />
+          Transmettre cette culture hors de la scène est pour nous une mission
+          d’importance pour populariser la musique à danser auprès d’un public
+          de curieux qui ne foulent pas toujours le plancher.
+          <br />
+          <br />
+        </p>
       </div>
       <div className="videos">
         {videos.map((item) => (
