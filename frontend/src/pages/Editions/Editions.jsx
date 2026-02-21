d28794224b80261bccf818dfc5b2137c13f7464a
kévin goujon <goujon.kevin@hotmail.fr>
2026-02-21 12:38:33 +0100

    test

--- frontend/src/pages/Editions/Editions.jsx
+++ frontend/src/pages/Editions/Editions.jsx
@@ -1,10 +1,10 @@
 //import './Editions.scss'
-import { useParams, useNavigate } from 'react-router-dom'
+/*import { useParams, useNavigate } from 'react-router-dom'*/
 function Editions() {
-  const { editionId } = useParams()
-  const navigate = useNavigate()
+  //const { editionId } = useParams()
+  //const navigate = useNavigate()
 
-  const selectedEdition = editions.find((e) => e._id === editionId)
+  //const selectedEdition = editions.find((e) => e._id === editionId)
 
   return <> Les Editions</>
 }
