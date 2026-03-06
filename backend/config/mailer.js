aa157afd85e2fbbc14f430cb883bee8406adf51a
kévin goujon <goujon.kevin@hotmail.fr>
2026-03-06 12:54:45 +0100

    instal rate-limit

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // 587 = STARTTLS
+  secure: true, // port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
