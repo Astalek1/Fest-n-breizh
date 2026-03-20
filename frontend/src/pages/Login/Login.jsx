93b15795a839e0a3b0691fa3e72272ffd8aff4f5
kévin goujon <goujon.kevin@hotmail.fr>
2026-03-20 16:45:30 +0100

    ajout du oneSubmit

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,10 @@
 import './Login.scss'
+import { useState } from 'react'
 
 function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <>
       <h1 className="title">connexion</h1>
@@ -8,7 +12,10 @@ function Login() {
         Cette section est réservée à l'équipe de Fest'n breizh.
       </p>
 
-      <form className="formulaire__connexion">
+      <form
+        className="formulaire__connexion"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <label htmlFor="username" className="formulaire__connexion--label">
           Nom d'utilisateur :
         </label>
@@ -18,6 +25,8 @@ function Login() {
           type="text"
           name="username"
           placeholder="nom de l'utilisateur"
+          value={username}
+          onChange={(e) => setUsername(e.target.value.trim())}
         />
         <label htmlFor="password" className="formulaire__connexion--label">
           Password :
@@ -28,6 +37,8 @@ function Login() {
           type="text"
           name="password"
           placeholder="mots de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button type="submit" className="formulaire__connexion--button">
