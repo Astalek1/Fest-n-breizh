6c7e25311c2ea4a47213f4caa2ca2d6e11e171a4
kévin goujon <goujon.kevin@hotmail.fr>
2026-04-07 14:51:56 +0200

    modif import files

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import videosRoutes from "./routes/videos.js";
 import partnersRoutes from "./routes/partners.js";
 import linksRoutes from "./routes/links.js";
 import contactRoutes from "./routes/contact.js";
-import filesRoutes from '../backend/routes/files.js';
+import filesRoutes from './routes/files.js';
 
 dotenv.config();
 
