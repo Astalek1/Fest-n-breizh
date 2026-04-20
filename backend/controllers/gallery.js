9b77c6eb5769f3a6e7c948c2e62eae260f4032e2
kévin goujon <goujon.kevin@hotmail.fr>
2026-04-20 12:25:44 +0200

    fixe req.file

--- backend/controllers/gallery.js
+++ backend/controllers/gallery.js
@@ -221,7 +221,7 @@ export const updatePhoto = async (req, res) => {
       if (body[field] !== undefined) filteredData[field] = body[field];
     }
 
-    if (req.file || body.media) {
+    if (req.file) {
       const cleanName = req.body.fileName?.trim()
         ? req.body.fileName.replace(/\s+/g, "-").toLowerCase()
         : body.title
