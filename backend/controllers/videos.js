b59cb603d86226172bf3ed2b01ad6e1839a9d382
kévin goujon <goujon.kevin@hotmail.fr>
2026-04-23 16:05:53 +0200

    fixe

--- backend/controllers/videos.js
+++ backend/controllers/videos.js
@@ -23,9 +23,8 @@ export const newVideo = async (req, res) => {
 
 // trouver toutes les vidéos//
 export const getAllVideos = async (req, res) => {
-  const videos = await Video.find().sort({ createdAt: -1 });
   try {
-    const videos = await Video.find();
+    const videos = await Video.find().sort({ createdAt: -1 });
     res.status(200).json(videos);
   } catch (error) {
     res.status(500).json("Erreur serveur, base de données inaccessible");
