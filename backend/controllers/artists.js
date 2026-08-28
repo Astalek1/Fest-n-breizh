bf03f36bbfb8a96198c8a83771a8023bbcdb6b57
kévin goujon <goujon.kevin@hotmail.fr>
2026-08-28 15:53:17 +0200

    patch images

--- backend/controllers/artists.js
+++ backend/controllers/artists.js
@@ -151,7 +151,10 @@ export const updateArtist = async (req, res, silent = false) => {
     if (mediaType) {
       filtered.mediaType = mediaType;
     }
-    const sentNewMedia = !!req.file || !!body.media || mediaType === "video";
+    const sentNewMedia =
+    !!req.file ||
+    mediaType === "video" ||
+    (mediaType === "logo" && isFileId(body.media));
     const oldImageId = existing.mediaFileId || null;
     const oldLogoId = existing.logoFileId || null;
 
