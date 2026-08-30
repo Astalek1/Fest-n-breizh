30494861f941976b27c4e27dad63399640d76bfe
kévin goujon <goujon.kevin@hotmail.fr>
2026-08-30 22:20:39 +0200

    test

--- frontend/src/pages/Editions/Editions.jsx
+++ frontend/src/pages/Editions/Editions.jsx
@@ -20,7 +20,7 @@ function Editions({ isEditing }) {
 
         setTimeout(() => {
           setEditions(sorted)
-        }, 100)
+        }, 500)
       })
       .catch((err) => console.error(err))
   }, [])
